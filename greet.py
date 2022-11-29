f7ac30f49eca468d5ad590e2d2db51669ea18395
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-11-29 16:52:18 -0500

    updated with docstring

--- greet.py
+++ greet.py
@@ -1,10 +1,15 @@
 def greet(name):
+    '''created a function that takes in a name in greet '''
     print('My name is ' + name)
 
 person = 'Sarah'
 def name_input():
+    '''created a function that will has name input'''
     print('Please input your name:\n')
     print(person)
+
+greet(name_input())
+'''takes in name input and will greet '''
 
 ask = input("What is your name")
 print(ask)
